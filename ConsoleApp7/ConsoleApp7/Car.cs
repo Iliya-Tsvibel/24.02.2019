3a457952d32ab6964096f87c31605c2e89c05ec3
Iliya-hacker <45923437+Iliya-hacker@users.noreply.github.com>
2019-02-26 16:12:20 +0200

    HW Serialization

---
+++ ConsoleApp7/ConsoleApp7/Car.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ConsoleApp7
+{
+    public class Car
+    {
+        public string Model { get; set; }
+        public int Brand { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+        private int codan;
+        protected int numberOfSeats;
+
+
+        public Car()
+        {
+
+        }
+        public Car(string fileName)
+        {
+            Car oto;
+            XmlSerializer iliyaxmlSerializer = new XmlSerializer(typeof(Car));
+            using (Stream file = new FileStream($@"c:\{fileName}\IliyaCar.xml", FileMode.Open)) //link
+            {
+                oto = iliyaxmlSerializer.Deserialize(file) as Car;
+            } // auto-closing the string
+            this.Model = oto.Model;
+            this.Brand = oto.Brand;
+            this.Year = oto.Year;
+            this.Color = oto.Color;
+        }
+
+        public Car(string model, int brand, int year, string color, int codan, int numberOfSeats)
+        {
+            Model = model;
+            Brand = brand;
+            Year = year;
+            Color = color;
+            this.codan = codan;
+            this.numberOfSeats = numberOfSeats;
+        }
+        public int MyCodan
+        {
+            get
+            {
+                return this.codan;
+            }
+        }
+        public int NumberOfSeats
+        {
+            get
+            {
+                return this.numberOfSeats;
+            }
+        }
+        protected void SetNumberOfSeats(int n)
+        {
+            this.numberOfSeats = n;
+        }
+        public static void SerializeACar(string fileName, Car car)
+        {
+            XmlSerializer myxmlSerializer = new XmlSerializer(typeof(Car));
+            using (Stream file = new FileStream($@"c:\{fileName}\IliyaCar.xml", FileMode.Create))
+            {
+                myxmlSerializer.Serialize(file, car);
+            } // auto-closing the string
+        }
+        public static void SerializeACarArray(string fileName, Car[] cars)
+        {
+            XmlSerializer myxmlSerializer = new XmlSerializer(typeof(Car[]));
+            using (Stream file = new FileStream($@"c:\{fileName}\IliyaCar.xml", FileMode.Create))
+            {
+                myxmlSerializer.Serialize(file, cars);
+            } // auto-closing the string
+        }
+        public static Car DeserializeACar(string fileName)
+        {
+            Car oto;
+            XmlSerializer myxmlSerializer = new XmlSerializer(typeof(Car));
+            using (Stream file = new FileStream($@"c:\{fileName}\IliyaCar.xml", FileMode.Open))
+            {
+                oto = myxmlSerializer.Deserialize(file) as Car;
+            }
+            return oto;
+        }
+        public static Car[] DeserializeACarArray(string fileName)
+        {
+
+            Car[] otos;
+            XmlSerializer myxmlSerializer = new XmlSerializer(typeof(Car[]));
+            using (Stream file = new FileStream($@"c:\{fileName}\IliyaCar.xml", FileMode.Open))
+            {
+                otos = myxmlSerializer.Deserialize(file) as Car[];
+            }
+            return otos;
+        }
+        //public bool CarCompare(string fileName)
+        //{
+          
+        //}
+
+        public override string ToString()
+        {
+            return $"Model of Car:{this.Model} Brand:{this.Brand} Year:{this.Year} Color:{this.Color} codan:{this.codan} Seats number:{this.numberOfSeats}";
+        }
+    }
+}
---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Car[] otos =
+            //    {new Car("mitzubisi", 6, 2015, "metalic", 6758, 6),
+            //     new Car("toyota",5,1600,"black",2500,7),
+            //     new Car("lada",5,2009,"red",2700,5)};
+
+            XmlSerializer myxmlSerializer = new XmlSerializer(typeof(Car[]));
+            //using (Stream file = new FileStream(@"c:\temp\IliyaCar.xml", FileMode.Create))
+            //{
+            //    myxmlSerializer.Serialize(file, otos);
+            //} // auto-closing the string
+            //PrintArray(otos);
+            Car[] otos2;
+
+            using (Stream file = new FileStream(@"c:\temp\IliyaCar.xml", FileMode.Open))
+            {
+                otos2 = myxmlSerializer.Deserialize(file) as Car[];
+            } // auto-closing the string
+            PrintArray(otos2);
+        }
+        static void PrintArray(Car[] c)
+        {
+            foreach (Car oto in c)
+            {
+                Console.WriteLine(oto);
+            }
+        }
+    }
+}
